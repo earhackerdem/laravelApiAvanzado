4420ff6372ed3c9850eecb296c6f04d026684884
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-04-08 14:39:42 -0600

    instalado laravel ui

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Auth::routes(['verify' => true]);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
