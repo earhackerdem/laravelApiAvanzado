5d919960a487326cf06bbfa1cbdb1a854aba60c2
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2023-04-08 20:35:15 -0600

    Excepciones HTTP personalizadas y debugging con Laravel Telescope

---
+++ database/migrations/2023_04_09_021922_add_is_admin_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_admin')->default(false);
+        });
+
+        $user = User::find(1);
+
+        if($user){
+            $user->is_admin = 1;
+            $user->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+        });
+    }
+};
